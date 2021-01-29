55de6c7456ee2d6b0ad3928d957e3b1abddc2355
f2janyway <55625423+f2janyway@users.noreply.github.com>
2021-01-29 13:05:35 +0900

    Update detect_created_files.sh
    vscode는 그냥 launch.json과 task.json 변경으로 대체

--- detect_created_files.sh
+++ detect_created_files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #1. ./ 는 현재 디렉토리
 MONITORDIR="./"
-#현재 디렉토리에서 생성 되는 파일,폴더를 관찰하고		         파일을 읽는다.
+#현재 디렉토리에서 생성 되는 파일,폴더를 관찰하고		        파일을 읽는다.
 inotifywait -m -r -e create --format '%w%f' "${MONITORDIR}" | while read FILENAME
 do 
 	echo "${FILENAME}"	
-	#           생기는 파일중에            확장자가 없으면
+	#           생기는 파일 중에            확장자가 없으면
 	if [[ ${FILENAME} == "./"* &&  ${FILENAME} != "./"*"."* ]]; then
 		# 해당 디렉토리에 있는 bin 디렉토리로 옮기고
 		mv ${FILENAME} bin/
