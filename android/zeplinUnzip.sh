826739c5c19236d9d2981a3268edd84f2c915676
f2janyway <55625423+f2janyway@users.noreply.github.com>
2021-04-06 16:45:00 -0700

    Create zeplinUnzip.sh
    for mac

---
+++ android/zeplinUnzip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#dir=~/Downloads/chrome_download/*
+dir=~/Downloads/chrome_download/*
+drawable=~/android/4mation_android/app/src/main/res
+for file in $dir
+do
+        echo "$file"
+        if [[ "$file" == *".zip" ]]; then
+                echo "${file}"
+                #                                               res에 drawable들
+을 압축푼다.
+                unzip "${file}" -d $drawable
+               # rm "${file}"
+        fi
+done
